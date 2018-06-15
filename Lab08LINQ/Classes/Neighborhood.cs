7012906152a146a21c6fe5b66f8170d23ad486b5
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-15 09:29:37 -0700

    Added base project and nuGet package

---
+++ Lab08LINQ/Classes/Neighborhood.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab08LINQ.Classes
+{
+    class Neighborhood
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Lab08LINQ/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Lab08LINQ.Classes;
+using Newtonsoft.Json;
+
+namespace Lab08LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //string path = "data.json";
+            //Neighborhood neighborhood = JsonConvert.DeserializeObject<Neighborhood>(path);
+            //Console.WriteLine(neighborhood.Name);
+
+
+        }
+    }
+}
